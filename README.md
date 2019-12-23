cab8ed9b7ea717bc7c49fb2bb3b23b59abc64849
Simo Simeunovic <simo.simeunovic@esome.com>
2019-12-23 15:16:40 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # vpnbookpwd
+
 Print current VPM Book password
+
+# Usage
+
+## Run command
+
+```
+$ npx vpnbookpwd
+```
+
+## Example output
+
+```
+Username: "vpnbook"
+Password: "rvukb43"
+```
---
+++ src/index.js
@@ -0,0 +1,39 @@
+const Tesseract = require("tesseract.js");
+const axios = require("axios");
+
+const BASE_URL = "https://www.vpnbook.com";
+
+async function resolvePasswordImageRelativeUrl(vpnBookUrl) {
+  const { data: html } = await axios.get(vpnBookUrl);
+  const start = html.indexOf('src="password.php');
+  const end = html.indexOf(">", start);
+  const sliced = html.slice(start, end);
+
+  const urlStart = sliced.indexOf('"');
+  const urlEnd = sliced.indexOf('"', start);
+  const link = sliced
+    .slice(urlStart, urlEnd)
+    .replace(new RegExp('"', "g"), "")
+    .trim();
+
+  return link;
+}
+
+async function imageToText(imageUrl) {
+  const { data } = await Tesseract.recognize(imageUrl, "eng", {
+    logger: () => {}
+  });
+
+  return data.text.trim();
+}
+
+(async () => {
+  const passwordImageUrl = await resolvePasswordImageRelativeUrl(BASE_URL);
+
+  const url = encodeURI(`${BASE_URL}/${passwordImageUrl}`);
+
+  const password = await imageToText(url);
+
+  console.log(`Username: "vpnbook"`);
+  console.log(`Password: "${password}"`);
+})();
